82751de0c45066af04705b23b81c34d39144a4db
Stephanie <stephanie.furlan94@gmail.com>
2020-11-17 16:21:06 +0100

    Layout

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ for (i = 1; i <= 100; i++) {
       console.log(i);
    }
 }
+
+
+
+var show = document.getElementById("show");
+show.addEventListener("click", function() {
+
+   document.getElementById("numbers").innerHTML = "";
+   var min = document.getElementById("min").value;
+   var max = document.getElementById("max").value;
+   var div1 = document.getElementById("div1").value;
+   var div2 = document.getElementById("div2").value;
+
+   document.getElementById("both").innerHTML = div1 + " and " + div2;
+   document.getElementById("first").innerHTML = div1;
+   document.getElementById("second").innerHTML = div2;
+
+   for (i = min; i <= max; i++) {
+      var itemData = document.createTextNode(i);
+      var listItem = document.createElement('li');
+      listItem.appendChild(itemData);
+      if (i % (div1 * div2) == 0) {
+         listItem.setAttribute("class", "blue");
+
+      } else if (i % div1 == 0) {
+         listItem.setAttribute("class", "green");
+         console.log("Fizz");
+      } else if (i % div2 == 0){
+         listItem.setAttribute("class", "yellow");
+         console.log("Buss")
+      }
+
+      document.getElementById("numbers").appendChild(listItem);
+   }
+
+
+})
